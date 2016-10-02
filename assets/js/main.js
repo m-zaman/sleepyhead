23d61c23de4a1f576fb851cef33bf2b1d6d9de8f
Zuhayeer Musa <zuhaz3@gmail.com>
2016-10-02 02:18:59 -0700

    updated css, js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
-	$('#myswitch').switchable({ checked_color: '#ffec82' });
-	$('#myswitch1').switchable({ checked_color: '#ffec82' });
+	$('#myswitch').switchable({ checked_color: '#ffec82', click: testing124 });
+	$('#myswitch1').switchable({ checked_color: '#ffec82', click: changed1 });
 
-	function addDevice() {
-		alert("Added new device for the ML");
+	function testing124(ev, checked) {
+		if (checked) {
+			$('#second').html("<b style='color: green'>ON</b>");
+		} else {
+			$('#second').html("<b style='color: red'>OFF</b>");
+		}
 	}
+
+	function changed1(ev, checked) {
+		if (checked) {
+			$('#third').html("<b style='color: green'>ON</b>");
+		} else {
+			$('#third').html("<b style='color: red'>OFF</b>");
+		}
+	}
+
+	setInterval(function() {
+		
+	}, 50000);
+
 });
 
